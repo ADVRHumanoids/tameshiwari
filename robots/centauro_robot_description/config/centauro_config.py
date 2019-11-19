f7456755b52ae67069659cdc92079640aea3e5fd
pta-janssen <pta.janssen2@gmail.com>
2019-11-19 10:12:36 +0100

    Added Centauro URDF functionality with chosing joints, retrieving homing joints states, joint limits and more.

--- robots/centauro_robot_description/config/centauro_config.py
+++ robots/centauro_robot_description/config/centauro_config.py
@@ -16,11 +16,16 @@ class HomePose():
         self.name = name or JointNames().getName()
         tree = ET.parse(filename_srdf)
         root = tree.getroot()
-        self.pose = pose or 'home'
+        if pose is None:
+            self.pose = 'home'
+        else:
+            self.pose = pose
+        # self.pose = pose or 'home'
+        # print pose
         # self.name = []
         self.value = []
         for child in root:
-            if child.tag == 'group_state' and child.attrib['name'] == 'home':
+            if child.tag == 'group_state' and child.attrib['name'] == pose:
                 for subchild in child:
                     if subchild.attrib['name'] in self.name:
                         # self.name += [subchild.attrib['name']]
@@ -72,10 +77,12 @@ class JointNames():
 class JointBounds():
     def __init__(self,name=None):
         self.name = name or JointNames().getName()
+        # print self.name
         tree = ET.parse(filename_limits)
         root = tree.getroot()
 
         joint_str = []
+        motor_str = []
         self.lower = []
         self.upper = []
         self.velocity = []
@@ -86,11 +93,25 @@ class JointBounds():
                 i = [x.isdigit() for x in joint].index(True)
                 num = int(joint[i])
                 string = joint[:i]+joint[i+1:]
+                # print string
                 joint_str += [string]
             except ValueError:
                 string = joint
+                # print string
                 joint_str += [string]
+            if "arm" in string:
+                motor = string
+                # print motor
+                motor_str += [motor]
+            else:
+                for child in root:
+                    if child.tag == 'motor' and child.attrib['name'] in string:
+                        motor = child.attrib['value']
+                        # print motor
+                        motor_str += [motor]
+
             for child in root:
+                # print child.tag
                 name = child.attrib['name']
                 if string in name and 'lower' in name:
                     row = re.findall("[-+]?\d+\.\d+", child.attrib['value'])
@@ -98,6 +119,20 @@ class JointBounds():
                 if string in name and 'upper' in name:
                     row = re.findall("[-+]?\d+\.\d+", child.attrib['value'])
                     self.upper += [float(row[num-1])]
+                if motor in name and "velocity" in name:
+                    if 'motor' in name:
+                        self.velocity += [float(child.attrib['value'])]
+                    else: 
+                        row = re.findall("[-+]?\d+\.\d+", child.attrib['value'])
+                        self.velocity += [float(row[num-1])]
+                    # print self.velocity[-1]
+                if motor in name and ("effort" in name or "torque" in name):
+                    if 'motor' in name:
+                        self.torque += [float(child.attrib['value'])]
+                    else: 
+                        row = re.findall("[-+]?\d+\.\d+", child.attrib['value'])
+                        self.torque += [float(row[num-1])]
+                    # print self.torque[-1]
 
     def printName(self):
         print self.name
@@ -111,19 +146,43 @@ class JointBounds():
     def getUpperBound(self):
         return self.upper
 
+    def getVelocityBound(self):
+        return self.velocity
+
+    def getTorqueBound(self):
+        return self.torque
+
 if __name__ == "__main__":
     # DEBUGGING
+
+    # Getting home vectors for full centauro
+    centauro = JointNames()
+    centauro.printName()
+    HomePose('Navvab_Parking_3',centauro.getName()).printValue()
+
+    leftarm = JointNames('arm1')
+    leftarm.printName()
+
+    # left_arm = JointNames('arm1')
+    # bounds = JointBounds(left_arm.getName())
+    # print bounds.getName()
+    # print bounds.getLowerBound()
+    # print bounds.getUpperBound()
+    # print bounds.getVelocityBound()
+    # print bounds.getTorqueBound()
+    # print len(bounds.getTorqueBound())
+
     # home = HomePose()
     # home.printName()
     # home.printPose()
     # home.printValue()
 
-    left_arm = JointNames('arm1')
-    # left_arm.printName()
-    home = HomePose(name=left_arm.getName())
-    home.printName()
-    # home.printPose()
-    home.printValue()
+    # left_arm = JointNames('arm1')
+    # # left_arm.printName()
+    # # home = HomePose(name=left_arm.getName())
+    # # home.printName()
+    # # # home.printPose()
+    # # home.printValue()
     # left_arm_bounds = JointBounds(left_arm.getName())
     # print left_arm_bounds.getLowerBound()
     # print left_arm_bounds.getUpperBound()
